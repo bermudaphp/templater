945f28d8b577b1b6ffba8f499787db2120be7804
Andrey <alestro@bk.ru>
2020-06-17 12:46:51 +0200

    Update RendererFactory.php

--- src/RendererFactory.php
+++ src/RendererFactory.php
@@ -4,19 +4,41 @@
 namespace Lobster\Template;
 
 
+use Psr\Container\ContainerInterface;
+
+
 /**
  * Class RendererFactory
  * @package Lobster\Template
  */
 class RendererFactory
 {
+    private ContainerInterface $container;
+    
+    public function __construct(ContainerInterface $container)
+    {
+        $this->container = $container;
+    }
+    
     /**
      * @param array $templates
      * @param string $ext
      * @return Renderer
      */
-    public function __invoke(array $templates = [], string $ext = 'phtml') : Renderer
+    public function __invoke() : Renderer
     {
-        return new Renderer($templates, $ext);
+        $config = $this->container->get('renderer');
+        
+        $renderer = new Renderer($config['templates'], $config['ext'] ?? 'phtml');
+        
+        if(($functions = $config['functions']) != [])
+        {
+            foreach($functions as $name => $func)
+            {
+                $renderer->getPlates()->registerFunction($name, $func);
+            }
+        }
+        
+        return $renderer;
     }
 }
